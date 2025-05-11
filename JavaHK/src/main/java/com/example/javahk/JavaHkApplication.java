6ab8eb268038ac35ea6e4933af3e141feb07dc13
qiuerOwO <108218606+maotougu@users.noreply.github.com>
2025-05-11 16:49:59 +0800

    springboot 配置

---
+++ JavaHK/src/main/java/com/example/javahk/JavaHkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javahk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHkApplication.class, args);
+    }
+
+}
---
+++ JavaHK/src/test/java/com/example/javahk/JavaHkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javahk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
