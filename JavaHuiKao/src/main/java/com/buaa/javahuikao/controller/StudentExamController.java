845effd8bc0ae88996446ba9c8973aa96025f74f
ZiqiangWu <wuziqiang@buaa.edu.cn>
2025-06-05 15:42:05 +0800

    显示学生端考试列表班级名

--- JavaHuiKao/src/main/java/com/buaa/javahuikao/controller/StudentExamController.java
+++ JavaHuiKao/src/main/java/com/buaa/javahuikao/controller/StudentExamController.java
@@ -1,11 +1,15 @@
 package com.buaa.javahuikao.controller;
 
+import com.buaa.javahuikao.entity.Exam;
+import com.buaa.javahuikao.service.ClassService;
 import com.buaa.javahuikao.service.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,9 +21,24 @@ import java.util.Map;
 public class StudentExamController {
     @Autowired
     private ExamService examService;
+    @Autowired
+    private ClassService classService;
 
     @PostMapping("/exams")
     public Object getStudentExams(@RequestBody Map<String, Object> requestBody) {
-        return examService.getStudentExams((Integer)requestBody.get("userId"), (String)requestBody.get("status"));
+        List<Map<String, Object>> results = new ArrayList<>();
+        examService.getStudentExams((Integer)requestBody.get("userId"), (String)requestBody.get("status")).forEach(exam -> {
+            // 获取考试对应的班级列表
+            results.add(Map.of(
+                "id", exam.getId(),
+                "name", exam.getName(),
+                "startTime", exam.getStartTime(),
+                "duration", exam.getDuration(),
+                "sumScore", exam.getSumScore(),
+                "questionCount", exam.getQuestionCount(),
+                "classNames", classService.getClassListByExamId(exam.getId())
+            ));
+        });
+        return results;
     }
 }
